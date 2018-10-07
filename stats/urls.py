d5618f6add124d7fb5218ddb2a09aade5ce7f03e
Peter Dolan <pjdhd7@mst.edu>
2018-10-07 11:08:08 -0500

    Added view to list history of all recorded measurements
    TODO:
      -Figure out if my DB query is incorrect, or if I have no data in the DB
      -Figure out why Pycharm does not like my attempt to aggregate data from the DB

--- stats/urls.py
+++ stats/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'stats'
 urlpatterns = [
     path('<int:user_id>/<int:stat_id>/', views.ShowStatsView.as_view(), name='show_stats'),
+    path('history/<int:user_id>/', views.ShowStatsHistoryView.as_view(), name='stats_history')
 ]
