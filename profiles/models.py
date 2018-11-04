086cbd13adaa39dd4c44c7304d5366770890015f
Eli Gaitley <ceg8b4@mst.edu>
2018-11-04 15:51:55 -0600

    Made some final backend adjustments.

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
 
 class Profile(models.Model):
@@ -22,6 +23,9 @@ class Profile(models.Model):
 
     class Meta:
         db_table = 'PROFILE'
+
+    def get_absolute_url(self):
+        return reverse('profiles:user_profile')
 
     def __str__(self):
         return self.user.username
