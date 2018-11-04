717b076410c90ba4144e998138b3280ce3966249
Eli Gaitley <ceg8b4@mst.edu>
2018-11-04 15:48:08 -0600

    Added a backend for user profile page and did some more backend touch ups.

--- marksWebapp/views.py
+++ marksWebapp/views.py
@@ -1,25 +1,17 @@
-from django.db.models import Max
+from django.db import transaction
 from django.shortcuts import render
-from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 
-from stats.models import Stats
+from .utils import get_most_recent_measurement_url
 
 
+index_decorators = [transaction.atomic]
+
+
+@method_decorator(index_decorators, name='dispatch')
 class Index(View):
     @staticmethod
     def get(request):
-        if request.user.is_authenticated:
-            most_rec_measure = Stats.objects.filter(user=request.user)
-            if most_rec_measure.exists():
-                max_id = most_rec_measure.aggregate(Max('stat_id'))['stat_id__max']
-                most_rec_measure = most_rec_measure.get(pk=max_id)
-            else:
-                most_rec_measure = None
-        else:
-            most_rec_measure = None
-        context = {
-            'title': 'Home',
-            'most_recent_url': most_rec_measure.get_absolute_url() if most_rec_measure else reverse('stats:no_stats'),
-        }
+        context = {'title': 'Home', 'most_recent_url': get_most_recent_measurement_url(request)}
         return render(request, 'main/index.html', context)
--- stats/views.py
+++ stats/views.py
@@ -1,18 +1,27 @@
 import csv
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import transaction
 from django.db.models import Avg
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
+
+from marksWebapp.utils import get_most_recent_measurement_url
 
 from .models import Stats
 
 
-# Create your views here.
+show_stats_decorators = [transaction.atomic]
+no_stats_decorators = [transaction.atomic]
+delete_stat_decorators = [transaction.atomic]
+stat_history_decorators = [transaction.atomic]
+export_stats_decorators = [transaction.atomic]
 
 
+@method_decorator(show_stats_decorators, name='dispatch')
 class ShowStatsView(LoginRequiredMixin, View):
     @staticmethod
     def get(request, stat_id):
@@ -29,19 +38,20 @@ class ShowStatsView(LoginRequiredMixin, View):
             'avg_current': '{0:.4f}'.format(current_measurements['current__avg']),
             'avg_voltage': '{0:.4f}'.format(voltage_measurements['voltage__avg']),
             'stat': stat_requested,
+            'most_recent_url': get_most_recent_measurement_url(request),
         }
         return render(request, 'stats/stat_view.html', context)
 
 
+@method_decorator(no_stats_decorators, name='dispatch')
 class NoStatsView(LoginRequiredMixin, View):
     @staticmethod
     def get(request):
-        context = {
-            'title': 'No Stats to show you!',
-        }
+        context = {'title': 'No Stats to show you!', 'most_recent_url': get_most_recent_measurement_url(request)}
         return render(request, 'stats/no_stats_to_show.html', context)
 
 
+@method_decorator(delete_stat_decorators, name='dispatch')
 class DeleteStatsView(LoginRequiredMixin, View):
     @staticmethod
     def get(request, stat_id):
@@ -49,21 +59,20 @@ class DeleteStatsView(LoginRequiredMixin, View):
         return redirect(reverse('stats:stats_history'))
 
 
+@method_decorator(stat_history_decorators, name='dispatch')
 class ShowStatsHistoryView(LoginRequiredMixin, View):
     @staticmethod
     def get(request):
         # Query the DB
         all_stats = Stats.objects.filter(user=request.user)
-
+        context = {'title': 'All Recorded Measurements', 'most_recent_url': get_most_recent_measurement_url(request)}
         if all_stats.exists():
             # Create a context with all of the results from the query
-            context = { "all_stats": all_stats }
-        else:
-            # Create an empty context. The HTML will take care of things.
-            context = {}
+            context['all_stats'] = all_stats
         return render(request, 'stats/stat_history.html', context)
 
 
+@method_decorator(export_stats_decorators, name='dispatch')
 class ExportStatsToExcelView(LoginRequiredMixin, View):
     @staticmethod
     def get(request):
