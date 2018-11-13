60f89efc1a83ea838d5c2361569c04afa39dd619
Peter Dolan <pjdhd7@mst.edu>
2018-11-13 16:20:17 -0600

    Added date filtering

--- stats/forms.py
+++ stats/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.widgets import SelectDateWidget
 
 choices = (
     ('less_than' , 'Less than'),
@@ -12,12 +13,15 @@ class filterForm(forms.Form):
     current_cb = forms.BooleanField(required=False)
     power_cb = forms.BooleanField(required=False)
 
-    date_time_select = forms.ChoiceField(choices=choices)
+    date_time_select = forms.ChoiceField(choices=(('before', 'Before'), ('after', 'After')))
     voltage_select = forms.ChoiceField(choices=choices)
     current_select = forms.ChoiceField(choices=choices)
     power_select = forms.ChoiceField(choices=choices)
 
-    date_time_tb = forms.DateTimeField(required=False)
+    date_time_tb = forms.DateTimeField(required=True, widget=SelectDateWidget(
+        # empty_label=("Choose Year", "Choose Month", "Choose Day"),
+        # empty_label=''
+    ))
     voltage_tb = forms.FloatField(required=False)
     current_tb = forms.FloatField(required=False)
     power_tb = forms.FloatField(required=False)
