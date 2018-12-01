aafbe38f9fb4967de0852d7e5fef3b38a37e4f4b
Eli Gaitley <ceg8b4@mst.edu>
2018-12-01 17:13:27 -0600

    Wrote POST request code (Pre Testing).

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.utils.decorators import method_decorator
 from django.views import View
 
@@ -28,15 +29,12 @@ class SettingsView(LoginRequiredMixin, View):
 
     def get(self, request):
         profile = request.user.profile
-        phase_1_pins = profile.phase_1_settings
-        phase_2_pins = profile.phase_1_settings
-        phase_3_pins = profile.phase_3_settings
         phase_1_initial = {}
         phase_2_initial = {}
         phase_3_initial = {}
-        self.process_initial_data(phase_1_pins, phase_1_initial)
-        self.process_initial_data(phase_2_pins, phase_2_initial)
-        self.process_initial_data(phase_3_pins, phase_3_initial)
+        self.process_initial_data(profile.phase_1_settings, phase_1_initial)
+        self.process_initial_data(profile.phase_2_settings, phase_2_initial)
+        self.process_initial_data(profile.phase_3_settings, phase_3_initial)
         context = {
             'title': 'Pin Settings for {}'.format(profile),
             'most_recent_url': get_most_recent_measurement_url(request),
@@ -45,3 +43,26 @@ class SettingsView(LoginRequiredMixin, View):
             'phase_3_form': PhaseForm(initial=phase_3_initial, prefix=self.PHASE_3),
         }
         return render(request, 'profiles/pin_settings.html', context)
+
+    def post(self, request):
+        profile = request.user.profile
+        phase_1_form = PhaseForm(request.POST, prefix=self.PHASE_1)
+        phase_2_form = PhaseForm(request.POST, prefix=self.PHASE_2)
+        phase_3_form = PhaseForm(request.POST, prefix=self.PHASE_3)
+        if phase_1_form.is_valid() and phase_2_form.is_valid() and phase_3_form.is_valid():
+            profile.phase_1_settings = {'pins': phase_1_form.cleaned_data.values()}
+            profile.phase_2_settings = {'pins': phase_2_form.cleaned_data.values()}
+            profile.phase_3_settings = {'pins': phase_3_form.cleaned_data.values()}
+            profile.save()
+            messages.success(request, 'Pin Configurations Successfully Stored.')
+            return redirect(reverse('index'))
+        else:
+            messages.error(request, 'Something Went Wrong On Server Side Processing.')
+            context = {
+                'title': 'Pin Settings for {}'.format(profile),
+                'most_recent_url': get_most_recent_measurement_url(request),
+                'phase_1_form': phase_1_form,
+                'phase_2_form': phase_2_form,
+                'phase_3_form': phase_3_form,
+            }
+            return render(request, 'profiles/pin_settings.html', context)
