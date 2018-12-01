b8590c79259611f78ce58da3dc5ebfe8e2e6ae00
Eli Gaitley <ceg8b4@mst.edu>
2018-12-01 15:10:14 -0600

    Completed backend get request for pin settings configuration.

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,13 +7,13 @@ from .models import Profile
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ['first_name', 'last_name', 'email']
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('status')
+        fields = ['status']
 
 
 class PhaseForm(forms.Form):
--- profiles/views.py
+++ profiles/views.py
@@ -6,16 +6,42 @@ from django.views import View
 
 from marksWebapp.utils import get_most_recent_measurement_url
 
+from .forms import PhaseForm
+
 
 settings_decorators = [transaction.atomic]
 
 
 @method_decorator(settings_decorators, name='dispatch')
 class SettingsView(LoginRequiredMixin, View):
+    PHASE_1 = 'Phase 1'
+    PHASE_2 = 'Phase 2'
+    PHASE_3 = 'Phase 3'
+
     @staticmethod
-    def get(request):
+    def process_initial_data(phase_pins, initial_settings):
+        if 'pins' in phase_pins:
+            count = 1
+            for pin_value in phase_pins['pins']:
+                initial_settings['pin{}'.format(count)] = pin_value
+                count += 1
+
+    def get(self, request):
+        profile = request.user.profile
+        phase_1_pins = profile.phase_1_settings
+        phase_2_pins = profile.phase_1_settings
+        phase_3_pins = profile.phase_3_settings
+        phase_1_initial = {}
+        phase_2_initial = {}
+        phase_3_initial = {}
+        self.process_initial_data(phase_1_pins, phase_1_initial)
+        self.process_initial_data(phase_2_pins, phase_2_initial)
+        self.process_initial_data(phase_3_pins, phase_3_initial)
         context = {
-            'title': 'Pin Settings for {}'.format(request.user.username),
+            'title': 'Pin Settings for {}'.format(profile),
             'most_recent_url': get_most_recent_measurement_url(request),
+            'phase_1_form': PhaseForm(initial=phase_1_initial, prefix=self.PHASE_1),
+            'phase_2_form': PhaseForm(initial=phase_2_initial, prefix=self.PHASE_2),
+            'phase_3_form': PhaseForm(initial=phase_3_initial, prefix=self.PHASE_3),
         }
         return render(request, 'profiles/user_profile.html', context)
