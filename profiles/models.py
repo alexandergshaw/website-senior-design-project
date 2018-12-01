502e84d0e7b6548b3359e8a169d4aebe7a701de3
Eli Gaitley <ceg8b4@mst.edu>
2018-12-01 14:32:30 -0600

    Made Custom JSONField to use in our database table.

--- profiles/models.py
+++ profiles/models.py
@@ -4,27 +4,28 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 
+from .fields import JSONField
+
 
 class Profile(models.Model):
-    UNDERGRAD = 'UN'
-    GRADUATE = 'GR'
-    PROFESSOR = 'PRO'
+    STUDENT = 'ST'
     ADMIN = 'AD'
     STATUS_CHOICES = (
-        (UNDERGRAD, 'Undergraduate Student'),
-        (GRADUATE, 'Graduate Student'),
-        (PROFESSOR, 'Professor'),
+        (STUDENT, 'Student User'),
         (ADMIN, 'Site Admin'),
     )
 
     user = models.OneToOneField(User, primary_key=True, db_column='USER_ID', on_delete=models.CASCADE)
     status = models.CharField(max_length=5, choices=STATUS_CHOICES, db_column='STATUS')
-    color_scheme = models.CharField(max_length=30, db_column='COLOR_SCHEME')
+    phase_1_settings = JSONField(null=True, blank=True, db_column='PHASE_1_SETTINGS')
+    phase_2_settings = JSONField(null=True, blank=True, db_column='PHASE_2_SETTINGS')
+    phase_3_settings = JSONField(null=True, blank=True, db_column='PHASE_3_SETTINGS')
 
     class Meta:
         db_table = 'PROFILE'
 
-    def get_absolute_url(self):
+    @staticmethod
+    def get_absolute_url():
         return reverse('profiles:user_profile')
 
     def __str__(self):
